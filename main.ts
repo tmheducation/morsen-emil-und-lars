ca3eb5fd5adad400d24147e29140636afffa2cd4
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-11 12:49:54 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
+let Wort = ""
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Wort = "" + Wort + "."
+    basic.setLedColor(0x00ffff)
     basic.showLeds(`
         . . . . .
         . # # # .
@@ -6,15 +9,22 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
         . # # # .
         . . . . .
         `)
-    basic.pause(500)
+    basic.pause(1000)
     basic.clearScreen()
+    basic.turnRgbLedOff()
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    basic.setLedColor(0x0000ff)
+    radio.sendString("" + (Wort))
+    basic.pause(100)
+    Wort = ""
+    basic.setLedColor(0xff0000)
+    music.playMelody("C E G C5 C5 G E C ", 120)
     basic.pause(1000)
     basic.turnRgbLedOff()
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Wort = "" + Wort + "-"
+    basic.setLedColor(0x00ff00)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -22,36 +32,39 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         . . . . .
         . . . . .
         `)
-    basic.pause(500)
+    basic.pause(1000)
     basic.clearScreen()
+    basic.turnRgbLedOff()
 })
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "-") {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    }
-    if (receivedString == ".") {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-        basic.pause(500)
-        basic.clearScreen()
-    }
-    if (receivedString == "_") {
-        basic.setLedColor(0x00ffff)
-        basic.pause(500)
-        basic.turnRgbLedOff()
+    music.playMelody("C5 A F C C F A C5 ", 120)
+    for (let zeichen of receivedString) {
+        if (zeichen == "-") {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(500)
+            basic.clearScreen()
+            basic.pause(100)
+        }
+        if (zeichen == ".") {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+            basic.pause(500)
+            basic.clearScreen()
+            basic.pause(100)
+        }
     }
 })
-radio.setGroup(17)
+basic.forever(function () {
+    radio.setGroup(17)
+})
